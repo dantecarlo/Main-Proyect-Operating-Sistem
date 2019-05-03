246cb0f8ffbb69752e10e76683e58344778ae75b
juango <jgonzalofh2898@gmail.com>
2019-05-03 13:59:38 -0500

    cambios adriana

--- pipe_pairs.c
+++ pipe_pairs.c
@@ -5,8 +5,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#define MAXSIZE     10
 
 #define MAX_BUF 10
+
+void die(char *s)
+{
+  perror(s);
+  exit(1);
+}
+
+struct msgbuf
+{
+    long    mtype;
+    char    mtext[MAXSIZE];
+};
+
 
 int main()
 {
@@ -54,12 +71,44 @@ int main()
 		{ //printf("parent\n");
 			// Child process closes up output side of pipe
 			close(fd[1]);
-
 			// Read in a string from the pipe
 			nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
 			if (strlen(readbuffer) > 0)
 				printf("Received in unnamed pipe: %s\n", readbuffer);
+            //codigo juango
+        int msqid;
+        int msgflg = IPC_CREAT | 0666;
+        key_t key;
+        struct msgbuf sbuf;
+        size_t buflen;
+
+        key = 4321;
+
+        if ((msqid = msgget(key, msgflg )) < 0)   //Get the message queue ID for the given key
+          die("msgget");
+
+        //Message Type
+        sbuf.mtype = 1;
+
+//        printf("Enter a message to add to message queue : ");
+//        scanf("%[^\n]",sbuf.mtext);
+        //        getchar();
+        sprintf(sbuf.mtext,"%s",readbuffer);//sbuf.mtext = readbuffer;
+        buflen = strlen(sbuf.mtext) + 1 ;
+
+        if (msgsnd(msqid, &sbuf, buflen, IPC_NOWAIT) < 0)
+        {
+            printf ("%d, %ld, %s, %d \n", msqid, sbuf.mtype, sbuf.mtext, (int)buflen);
+            
+            die("msgsnd");
+        }
+
+        else
+            printf("Message Sent\n");
 		}
+
+//end juango
+
 		sleep(2);
 	}
 	return 0;
