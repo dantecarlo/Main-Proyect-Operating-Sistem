27038bd27f4216d20449f387f9bc9235ea08eb3b
adrianaguu <adriana.gutierrez@ucsp.edu.pe>
2019-06-27 22:33:55 -0500

    SharedMemory

--- makefile
+++ makefile
@@ -2,7 +2,7 @@ all:
 	g++ -o start.exe start.cpp -lpthread
 	g++ -o pipe.exe pipe.cpp -lpthread
 	g++ -o sh.exe sh.cpp -lpthread
-	gcc -o sh_par_client.exe sh_par_client.c 
+	g++ -o sh_client.exe sh_client.cpp -lpthread
 
 clean:
 	rm -rf *.txt *.exe
--- sh.cpp
+++ sh.cpp
@@ -7,8 +7,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
 #include <sys/msg.h>
 #include <stack>
 #include <string>
@@ -21,10 +19,16 @@ stack<int> scheduler;
 int msqid;
 key_t key1;
 struct msgbuf rcvbuffer;
-
+int shmid;
+key_t key;
+char *shm, *s;
+void die(char *s)
+{
+    perror(s);
+    exit(1);
+}
 void encolar()
 {
-
     key1 = 4321;
 
     if ((msqid = msgget(key1, 0666)) < 0)
@@ -37,10 +41,41 @@ void encolar()
     scheduler.push(num);
     printf("message recieved %s\n", rcvbuffer.mtext);
 }
-void die(char *s)
+
+void desencolar()
 {
-    perror(s);
-    exit(1);
+    key = 5679;
+
+    // Create the segment.
+
+    if ((shmid = shmget(key, MAXSIZE, IPC_CREAT | 0666)) < 0)
+    {
+        perror("shmget");
+        exit(1);
+    }
+
+    // Now we attach the segment to our data space.
+
+    if ((shm = (char *)shmat(shmid, NULL, 0)) == (char *)-1)
+    {
+        perror("shmat");
+        exit(1);
+    }
+
+    /*
+     * Now put some things into the memory for the
+     * other process to read.
+     */
+    s = shm;
+    char buf[MAXSIZE];
+    sprintf(buf,"%d",scheduler.top());
+    int len = strlen(buf);
+    for (int i = 0; i < len; i++)
+        *s++ = buf[i];
+    *s = NULL;
+    printf("wrotten in sh: %s\n",buf);
+    while (*shm != '*')
+        sleep(0.5);
 }
 
 struct msgbuf1
@@ -55,55 +90,9 @@ main()
     {
 
         thread e(encolar);
-
-        //char c;
-        int shmid;
-        key_t key;
-        char *shm, *s;
-        /*
-     * We'll name our shared memory segment
-     * "5678".
-     */
-        key = 5679;
-
-        /*
-     * Create the segment.
-     */
-        if ((shmid = shmget(key, MAXSIZE, IPC_CREAT | 0666)) < 0)
-        {
-            perror("shmget");
-            exit(1);
-        }
-
-        /*
-     * Now we attach the segment to our data space.
-     */
-        if ((shm = (char *)shmat(shmid, NULL, 0)) == (char *)-1)
-        {
-            perror("shmat");
-            exit(1);
-        }
-
-        /*
-     * Now put some things into the memory for the
-     * other process to read.
-     */
-        s = shm;
-
-        int len = strlen(rcvbuffer.mtext);
-        //        printf("len %d", len);
-        for (int i = 0; i < len; i++)
-            *s++ = rcvbuffer.mtext[i];
-        *s = NULL;
-
-        /*
-     * Finally, we wait until the other process 
-     * changes the first character of our memory
-     * to '*', indicating that it has read what 
-     * we put there.
-     */
-        /*while (*shm != '*')
-            sleep(1);*/
+        thread d(desencolar);
+        e.join();
+        d.join();
     }
     exit(0);
 }
